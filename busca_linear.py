9c273a9c4eb9ec49172fd5edab692169b0560f46
Pierre Vieira <pierrevieiraggg@gmail.com>
2020-02-12 21:21:18 -0300

    [+] busca linear

---
+++ busca_linear.py
@@ -0,0 +1,22 @@
+"""
+Autor: Pierre Vieira
+"""
+
+
+def busca_linear(lista, elemento):
+    """
+    Complexidade -> O(n)
+    :param lista: Onde será feita a busca.
+    :param elemento: elemento a ser buscado.
+    :return: índice do elemento caso ele esteja na lista, None caso contrário.
+    """
+    for c in range(len(lista)):
+        if lista[c] == elemento:
+            return c
+    return None
+
+
+"""
+Essa maneira não é a mais eficiente para realizar a busca. Caso os elementos estejam ordenados, o mais aconselhável é
+utilizar o algoritmo de busca binária (complexidade O(log(n)).
+"""
